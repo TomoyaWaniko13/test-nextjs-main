2615e6103337d4d9e8988a8b37222f207dca9137
TomoyaWaniko13 <144481877+TomoyaWaniko13@users.noreply.github.com>
2025-01-10 18:20:37 +0900

    initial commit

---
+++ src/components/providers/ToastProvider/ToastContext.tsx
@@ -0,0 +1,30 @@
+import { createContext } from "react";
+
+export type ToastStyle = "succeed" | "failed" | "busy";
+
+export type ToastState = {
+  isShown: boolean;
+  message: string;
+  style: ToastStyle;
+};
+
+export const initialState: ToastState = {
+  isShown: false,
+  message: "",
+  style: "succeed",
+};
+
+// createContext API で Context object を作成
+export const ToastStateContext = createContext(initialState);
+
+export type ToastAction = {
+  showToast: (state?: Partial<Omit<ToastState, "isShown">>) => void;
+  hideToast: () => void;
+};
+
+export const initialAction: ToastAction = {
+  showToast: () => {},
+  hideToast: () => {},
+};
+
+export const ToastActionContext = createContext(initialAction);
